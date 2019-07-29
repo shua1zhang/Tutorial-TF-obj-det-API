ae1758c81734ccb043ee95adb5e681a6cdaa2964
zhangshuai <shuai.zhang@deephi.tech>
2019-07-29 16:31:30 +0800

    update scripts

--- 0_get_dataset.sh
+++
@@ -1,7 +0,0 @@
-#Download pet dataset from web
-DATASET_DIR='dataset'
-
-wget -P ${WORKSPACE}/${DATASET_DIR} http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
-wget -P ${WORKSPACE}/${DATASET_DIR} dataset http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
-tar -xvf ${WORKSPACE}/${DATASET_DIR}/images.tar.gz
-tar -xvf ${WORKSPACE}/${DATASET_DIR}/annotations.tar.gz
